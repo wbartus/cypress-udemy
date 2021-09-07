6a292d513c5f85e953e3b568ea8ff2d496ea2eb5
Bartosz Wezgraj <wezgraj@gmail.com>
2021-09-07 14:34:10 +0200

    checkboxes

--- cypress/integration/1-getting-started/test.js
+++ cypress/integration/1-getting-started/test.js
@@ -3,5 +3,8 @@
 describe("Test Suit", function () {
     it("Test Case", function () {
         cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+        cy.get('#checkBoxOption1').check()
+        cy.get('#checkBoxOption1').uncheck()
+        cy.get('#checkBoxOption1').should('not.be.checked')
     })
 })
