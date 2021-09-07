b3deafbd871e0b7f518c523ba7a2265bd078e914
Bartosz Wezgraj <wezgraj@gmail.com>
2021-09-07 13:54:30 +0200

    Test Suit

---
+++ cypress/integration/1-getting-started/test.js
@@ -0,0 +1,7 @@
+// <reference types="cypress" />
+
+describe("Test Suit", function () {
+    it("Test Case", function () {
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+    })
+})
