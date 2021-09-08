b3cb4d59356242cf8b09438285d280ac109f8c57
Bartosz Wezgraj <wezgraj@gmail.com>
2021-09-08 14:32:18 +0200

    Checkboxes and dropdowns

--- cypress/integration/1-getting-started/test.js
+++ cypress/integration/1-getting-started/test.js
@@ -3,8 +3,22 @@
 describe("Test Suit", function () {
     it("Test Case", function () {
         cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+
+        //Checkbox
         cy.get('#checkBoxOption1').check()
-        cy.get('#checkBoxOption1').uncheck()
-        cy.get('#checkBoxOption1').should('not.be.checked')
+        cy.get('#checkBoxOption1').uncheck().should('not.be.checked').and('have.value', 'option1')
+        
+        cy.get('input[type="checkbox"]').check(['option2', 'option3'])
+
+        //Static dropdown
+        cy.get('select').select('option3').should('have.value', 'option3')
+
+        //Dynamic dropdowm
+        cy.get('#autocomplete').type('ind').get('.ui-menu-item div').each(($el, index, $list) => {
+            if ($el.text()=='India') {
+                cy.wrap($el).click()
+            }
+        })
+        cy.get('#autocomplete').should('have.value', 'India')
     })
 })
